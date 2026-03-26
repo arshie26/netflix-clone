1ea592e4305f2f2f71db7c9cb638ed924dce4502
arshie26 <arshtist@gmail.com>
2026-03-26 16:44:54 -0400

    Netflix Hero

---
+++ src/netflix fails.jsx
@@ -0,0 +1,32 @@
+
+/* NAVBAR STYLE: COME BACK TO THIS */
+
+.navbar-left{
+    display: flex;
+}
+
+.navbar-left ul{
+    display: flex;
+}
+
+.navbar-left li{
+    margin-right: 40px;
+    padding-right: 24px;
+}
+
+
+.navbar-left{
+    display: flex;
+    align-items: center;
+    gap: 50px;
+}
+
+.navbar-left ul{
+    display: flex;
+    gap: 20px;
+    list-style: none;
+}
+
+.navbar-left img{
+    width: 90px;
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import './Home.css'
+import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
 
 const Home = () => {
   return (
     <div>
-      
+      <Navbar />
+      <div className="hero">
+        <img src={hero_banner} className='banner__img'/>
+        <div className='hero__caption'>
+          <img src={hero_title} className='caption__img'/>
+          <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis id facilis
+             explicabo perferendis inventore voluptatem culpa harum, consequatur similique ducimus 
+             eligendi reiciendis quibusdam magnam. Placeat velit cum dignissimos itaque deserunt?
+          </p>
+          <div className="hero__btns">
+            <button className="btn"><img src={play_icon} alt="" />Play</button>
+            <button className="btn dark-btn"><img src={info_icon} alt="" />Info</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
